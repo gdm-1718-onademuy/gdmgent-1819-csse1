974da19f4ae76a3972a6b688593c97c81e8f9d44
hektormisplon <hektormisplon@users.noreply.github.com>
2018-11-08 21:21:41 +0100

    Opdracht03_01

---
+++ opdrachten/opdracht3/01/Opdracht3_01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace opdracht3_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string,double> productList = new SortedList<string,double>();
+            productList.Add("Cola 25cl", 1);
+            productList.Add("Cola 33cl", 1.5);
+            productList.Add("Witte wijn", 2);
+            productList.Add("Pils", 3);
+            productList.Add("Toast", 5);
+            productList.Add("Kaasplank", 4);
+            
+            foreach(KeyValuePair<string,double> product in productList )
+            {
+                Console.WriteLine("product: {0}, price: {1}", product.Key , product.Value );
+            }
+            
+            if(productList.ContainsKey("Cola 25cl")) 
+            {
+                Console.WriteLine("The item you requested is available");
+            }
+        }
+    }
+}
---
+++ opdrachten/opdracht3/02/Opdracht3_02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace opdracht3_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program productList = new Program();
+
+            Queue orderQueue = new Queue();
+            orderQueue.Enqueue("table1");
+            orderQueue.Enqueue("table2");
+            orderQueue.Enqueue("table3");
+            orderQueue.Enqueue("table4");
+
+            object[] orders;
+
+            for(int i = 0; i < orderQueue.Count; i++) {
+                Console.WriteLine(orderQueue.Dequeue());
+                orders = orderQueue.ToArray();
+                Console.WriteLine("Your Orders:");
+                foreach (var order in orders)
+                {
+                    Console.WriteLine(order);
+                }
+            }      
+        }
+    }
+}
