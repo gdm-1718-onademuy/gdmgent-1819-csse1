7a5af706ec8da9e023adca1e0521654be50e4ad9
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-16 17:59:09 +0100

    Naming

--- opdrachten/opdracht5/deel3_singletonPattern/SingletonDbConnect.cs
+++ opdrachten/opdracht5/deel3_singletonPattern/SingletonDbConnect.cs
@@ -1,20 +1,20 @@
-public sealed class SingletonDbConnect
+public sealed class Singleton
 {
-    private static SingletonDbConnect instance;
+    private static Singleton instance;
 
     // make sure constructor is only available from within this class
-    private SingletonDbConnect()
+    private Singleton()
     {
 
     }
-    public static SingletonDbConnect Instance
+    public static Singleton Instance
     {
         // make sure there is only one instance
         get
         {
             if (instance == null)
             {
-                instance = new SingletonDbConnect();
+                instance = new Singleton();
             }
             return instance;
         }
